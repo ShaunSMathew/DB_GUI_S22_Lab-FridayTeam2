25160631527cd6facde3a43295a76b725d041cbb
Shaun Mathew <shaunsanthosh@gmail.com>
2022-03-30 15:46:53 -0500

    Update LandingPage.jsx

--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -3,10 +3,10 @@ export const LandingPage = () => {
     return (
         <>
             <h1>Farmer's Market</h1>
-            <h4>Welcome to the Farmer's Market, a place where farmers can easily sell goods to the restraunts that need them.</h4>
+            <h5>Welcome to the Farmer's Market, a place where farmers can easily sell goods to the restraunts that need them.</h5>
             <p>To access the site, please create a new account or sign in to an existing account.</p>
-            <button>Sign Up</button>
-            <button>Log In</button>
+            <h3>Sign Up</h3>
+            <h3>Log In</h3>
         </>
     );
 }
