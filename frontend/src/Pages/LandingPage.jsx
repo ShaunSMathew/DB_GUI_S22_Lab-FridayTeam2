ddc9e62d7c617aa5eed45544cfb9221cb461e32a
Shaun Mathew <shaunsanthosh@gmail.com>
2022-03-30 15:37:15 -0500

    Temporary landing page with useless buttons

--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from 'react';
 export const LandingPage = () => {
     return (
-            <h1>Farmers Market</h1>
-    )
+        <>
+            <h1>Farmer's Market</h1>
+            <h4>Welcome to the Farmer's Market, a place where farmers can easily sell goods to the restraunts that need them.</h4>
+            <p>To access the site, please create a new account or sign in to an existing account.</p>
+            <button>Sign Up</button>
+            <button>Log In</button>
+        </>
+    );
 }
