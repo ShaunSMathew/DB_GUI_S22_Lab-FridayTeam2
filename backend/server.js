88a0a0665a44ede9c393f0e72ae73b294bec558c
llmalone <78106359+llmalone@users.noreply.github.com>
2022-04-13 17:43:06 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const { log, ExpressAPILogMiddleware } = require('@rama41222/node-logger');
 // const mysqlConnect = require('./db');
 const routes = require('./routes');
+const restOwnerRoutes = require('./routes/rest_owner');
 
 // set up some configs for express.
 const config = {
@@ -29,6 +30,7 @@ app.use(ExpressAPILogMiddleware(logger, { request: true }));
 
 //include routes
 routes(app, logger);
+app.use('/owners', restOwnerRoutes );
 
 // connecting the express object to listen on a particular port as defined in the config object.
 app.listen(config.port, config.host, (e) => {
