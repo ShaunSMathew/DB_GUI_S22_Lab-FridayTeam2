3cc78a6acdfba240146ee918c442ddaa405f3f8a
Shaun Mathew <shaunsanthosh@gmail.com>
2022-04-20 16:19:25 -0500

    Addin text field component

---
+++ frontend/src/Common/TextField.jsx
@@ -0,0 +1,15 @@
+export const TextField = ({ label, value, setValue,type }) => {
+  return (
+    <div className="form-group mb-3">
+      <label htmlFor="value">{label}</label>
+      <input
+        type={type}
+        id="value"
+        name="value"
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+        className="form-control"
+      />
+    </div>
+  );
+};
