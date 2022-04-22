8b54cb8531a139f2acf0d561f0c735a6234a8071
Ziyu Sun <78039863+ziyusun00@users.noreply.github.com>
2022-04-22 16:12:53 -0500

    Update order.js

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,47 +1,27 @@
 const express = require('express');
-const farmer = require('../models/farmer');
+const product = require('../models/product');
 const rest_owner = require('../models/rest_owner');
 const order = require('../models/order');
 const router = express.Router();
 
 router.post('/', async (req, res, next) => {
     try {
-        const user = req.user;
         const body = req.body;
-        console.log(body);
-        const farmer = await farmer.findUserByUsername(user.username);
+        const user = req.user;
         const rest_owner = await rest_owner.findUserByUsername(user.username);
-        farmer;
-        rest_owner;
-        result = await order.createNewOrder(farmer.id, rest_owner.id);
+        const product = await product.findProductByID(body.product_id);
+        if(product.amount < body.amount){
+            const err = { message: "amount not enough" };
+            throw err;
+        }
+        const result = await order.createNewOrder(body.farmer_username, rest_owner.username, body.product_id, body.amount, body.tip);
+        const update = await product.updateAmount(body.amount);
         res.status(201).json(result);
     } catch (err) {
-        console.error('Failed to create new product:', err);
+        console.error('Failed to create new order:', err);
         res.status(500).json({ message: err.toString() });
     }
     next();
 });
 
-// GET /order?farmer=[farmer_id]&rest_owner=[rest_owner_id]
-router.get('/', async (req, res, next) => {
-    try {
-        const farmerQuery = req.query.farmer;
-        const rest_ownerQuery = req.query.rest_owner;
-        var result;
-        if (rest_ownerQuery === undefined && farmerQuery === undefined) {
-            result = await order.findAllOrder();
-        }else if(!(rest_ownerQuery) === undefined && !(farmerQuery === undefined)){
-            result = await order.findOrderByBothID(farmerQuery, rest_ownerQuery);
-        }else if(!(rest_ownerQuery === undefined)){
-            result = await order.findOrderByRestOwnerID(rest_ownerQuery);
-        }else{
-            result = await order.findOrderByFarmerID(farmerQuery);
-        }
-        res.status(201).json(result);
-    } catch (err) {
-        console.error('Failed to create new product:', err);
-        res.status(500).json({ message: err.toString() });
-    }
-    next();
-});
 module.exports = router;
