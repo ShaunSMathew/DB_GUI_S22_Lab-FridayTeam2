07eaf083275e800e2c5a29a3ecef34e68bdcee7c
llmalone <78106359+llmalone@users.noreply.github.com>
2022-04-26 16:38:10 -0500

    Added schedule routes

--- backend/models/schedule.js
+++ backend/models/schedule.js
@@ -8,6 +8,29 @@ const getScheduleByFarmer = async (username) => {
     return result;
 };
 
+const postEntry = async (date, time, entry, farmer_username) => {
+    const query = knex(SCHEDULE_TABLE).insert({date, time, entry, farmer_username});
+    const resultID = await query;
+    const result = knex(SCHEDULE_TABLE).where('id', resultID);
+    return result;
+};
+
+const putEntry = async (date, time, entry, farmer_username) => {
+    const query = knex(SCHEDULE_TABLE).where('id', id).update({date, time, entry, farmer_username});
+    const result = await query;
+    const product = knex(SCHEDULE_TABLE).where('id', result);
+    return product;
+};
+
+const deleteEntry = async (id) => {
+    const query = knex(SCHEDULE_TABLE).where('id', id).del();
+    const result = await query;
+    return result;
+};
+
 module.exports = {
-    getScheduleByFarmer
+    getScheduleByFarmer,
+    postEntry,
+    putEntry,
+    deleteEntry
 }
