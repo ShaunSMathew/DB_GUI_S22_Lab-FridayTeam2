6161b36b3323cf09089ba06604e2932fcafbb471
Shaun Mathew <shaunsanthosh@gmail.com>
2022-04-29 13:36:28 -0500

    Update farmer.js

--- backend/models/farmer.js
+++ backend/models/farmer.js
@@ -16,76 +16,21 @@ const findUserByUsername = async (username) => {
   return result;
 };
 
-<<<<<<< HEAD
-const updateProfile = async (username, street_address, city, state, zip, phone_num) => {
+const updateProfile = async (username, street_address, city, state, zip, phone_num, profile_pic) => {
   const query = await knex(FARMER_TABLE).where({ username }).update({
     street_address,
     city,
     state,
     zip,
     phone_num,
+    profile_pic,
   });
-  const result = await knex(FARMER_TABLE).where({ username });
-  return result;
-};
-
-const changeAddress = async (body, farmerUser, street_address) => {
-  //const newAddress = body.address;
-
-  const changeAddy = knex(FARMER_TABLE).where({ username: farmerUser }).update({});
-  console.log(`Raw query for changeAddress: `, changeAddy.toString());
-  const result = await changeAddy;
-  return result;
-};
-
-const deleteAddress = async (body, farmerUser) => {
-  const address = knex(FARMER_TABLE).where({ username: farmerUser }).update({ street_address: " ", city: " ", state: " ", zip: " " });
-  //Not sure if this is how to do this-- don't actually want to delete anything from database
-  const result = await address;
-  return result;
-};
-const searchFarmerByLocation = async (street, city, state, zip) => {
-  const query = knex(FARMER_TABLE)
-    .where((builder) => {
-      if (street) {
-        builder.where("street_address", street);
-      }
-    })
-    .where((builder) => {
-      if (city) {
-        builder.where("city", city);
-      }
-    })
-    .where((builder) => {
-      if (state) {
-        builder.where("state", state);
-      }
-    })
-    .where((builder) => {
-      if (zip) {
-        builder.where("zip", zip);
-      }
-=======
-const updateProfile = async (username, street_address, city, state, zip, phone_num, profile_pic) => {
-    const query = await knex(FARMER_TABLE).where({ username }).update({
-        street_address, city, state, zip, phone_num, profile_pic
->>>>>>> main
-    });
   const result = await query;
   return result;
 };
 
-<<<<<<< HEAD
-module.exports = {
-  createNewFarmer,
-  findUserByUsername,
-  changeAddress,
-  deleteAddress,
-  updateProfile,
-  searchFarmerByLocation,
-=======
 const deleteAddress = async(body, farmerUser) =>{
-    const address = knex(FARMER_TABLE).where({username:farmerUser}).update({street_address: " ", city : " ", state: " ", zip: " "}); 
+    const address = knex(FARMER_TABLE).where({ username: farmerUser }).update({ street_address: " ", city: " ", state: " ", zip: " " });
     //Not sure if this is how to do this-- don't actually want to delete anything from database
     const result = await address;
     return result;
@@ -99,7 +44,7 @@ const changePicture = async (body, farmerUser) => {
 };
 
 const deletePicture = async(farmerUser) =>{
-    const deletePic = knex(FARMER_TABLE).where({username:farmerUser}).update({picture: " "}); 
+    const deletePic = knex(FARMER_TABLE).where({ username: farmerUser }).update({ picture: " " });
     const result = await deletePic;
     return result;
 };
@@ -136,7 +81,7 @@ const addNumRatings = async(username) =>{
         farmer[0].num_of_ratings = 0;
     }
     const num_of_ratings = farmer[0].num_of_ratings + 1;
-    const qurey = knex(FARMER_TABLE).where({username}).update({num_of_ratings: num_of_ratings}); 
+    const qurey = knex(FARMER_TABLE).where({ username }).update({ num_of_ratings: num_of_ratings });
     console.log('addNumRatings:',qurey);
     const result = await qurey;
     return result;
@@ -148,22 +93,21 @@ const addSumRatings = async(username, rating) =>{
         farmer[0].ratings_sum = 0;
     }
     const ratings_sum = farmer[0].ratings_sum + rating;
-    const qurey = knex(FARMER_TABLE).where({username}).update({ratings_sum: ratings_sum}); 
+    const qurey = knex(FARMER_TABLE).where({ username }).update({ ratings_sum: ratings_sum });
     console.log('addNumRatings:',qurey);
     const result = await qurey;
     return result;
 }
 
 module.exports = {
-    createNewFarmer,
-    findUserByUsername,
-    changeAddress,
-    deleteAddress,
-    changePicture,
-    deletePicture,
-    updateProfile,
-    searchFarmerByLocation,
-    addNumRatings,
-    addSumRatings
->>>>>>> main
+  createNewFarmer,
+  findUserByUsername,
+  changeAddress,
+  deleteAddress,
+  changePicture,
+  deletePicture,
+  updateProfile,
+  searchFarmerByLocation,
+  addNumRatings,
+  addSumRatings,
 };
